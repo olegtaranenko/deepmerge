69b0e3ebc842a4267dcc302af922a80e6dfc6d70
Oleg Taranenko MP <olegtaranenko@gmail.com>
2023-11-22 05:15:52 +0100

    after build

--- dist/umd.js
+++ dist/umd.js
@@ -2,7 +2,7 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.deepmerge = factory());
-})(this, (function () { 'use strict';
+}(this, (function () { 'use strict';
 
 	var isPlainObj = value => {
 		if (Object.prototype.toString.call(value) !== '[object Object]') {
@@ -131,4 +131,4 @@
 
 	return deepmerge_1;
 
-}));
+})));
